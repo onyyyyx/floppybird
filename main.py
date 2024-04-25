85f48a19754829067c3656b57a6c4fb4150d5646
Onyx <shadowangel404@proton.me>
2024-04-25 11:50:49 +0200

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,24 +1,32 @@
-def draw(y: int, x: int):
+def draw(y, x):
     on=list(range(0,x))+list(range(x+1,5))
+    for i in on:
+        led.plot(y, i)
 
 def menu():
     led.set_brightness(0)
     basic.show_leds("""
-        . . . . .
-        . # . # .
-        # . . . #
-        . # . # .
-        . . . . .
-        """)
+    . . . . .
+    . # . # .
+    # . . . #
+    . # . # .
+    . . . . .
+    """)
     while not (input.button_is_pressed(Button.A) or input.button_is_pressed(Button.B)):
-        for i in range(256):
+        i = 0
+        while i < 256:
             led.set_brightness(i)
             basic.pause(1)
-            if input.button_is_pressed(Button.A) or input.button_is_pressed(Button.B): break
-        for i in range(255,-1,-1):
+            if input.button_is_pressed(Button.A) or input.button_is_pressed(Button.B):
+                break
+            i += 1
+        i = 255
+        while i > -1:
             led.set_brightness(i)
             basic.pause(1)
-            if input.button_is_pressed(Button.A) or input.button_is_pressed(Button.B): break
+            if input.button_is_pressed(Button.A) or input.button_is_pressed(Button.B):
+                break
+            i += -1
     basic.clear_screen()
 
-menu()
+draw(2,2)
